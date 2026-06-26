44218b98d23cdd02fd8af1721317a8a805119f82
akiko <fleurs24lirs@gmail.com>
2026-06-26 16:36:30 +0900

    PHP（上段）とHTML（下段）で記載を分けた

--- price_calculator.php
+++ price_calculator.php
@@ -1,36 +1,54 @@
+<?php
+$productName = "ノートパソコン"; //商品名
+$price = 80000; //単価
+$quantity = 2; //数量
+$taxRate = 0.1; //消費税率
+
+$subtotal = $price * $quantity; //小計
+$consumptionRate = $subtotal * $taxRate; //消費税（10%）
+$total = $subtotal + $consumptionRate; //合計金額
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHPの基礎</title>
 </head>
-
 <body>
-    <?php
-    $product_name = "ノートパソコン";
-    $price = 80000;
-    $quantity = 2;
-    $tax_rate = 0.1;
 
-    $subtotal = $price * $quantity;
-    $tax_amount = $subtotal * $tax_rate;
-    $total = $subtotal + $tax_amount;
-    ?>
+    <div>
+        <label for="productName">商品名: </label>
+        <?= $productName; ?>
+    </div>
 
-    <!-- <?php echo "商品名: $product_name";?></br>
-    <?php echo "単価: $price 円";?></br>
-    <?php echo "数量: $quantity 個";?></br>
-    <?php echo "小計: $subtotal 円";?></br>
-    <?php echo "消費税(10%): $tax_amount 円";?></br>
-    <strong><?php echo "合計金額: $total 円";?></strong></br> -->
+    <div>
+        <label for="price">単価: </label>
+        <?= number_format($price); ?>円
+    </div>
 
-    <p>商品名: <?= $product_name ?></p>
-    <p>単価: <?= $price ?>円</p>
-    <p>数量: <?= $quantity ?>個</p>
-    <p>小計: <?= $subtotal ?>円</p>
-    <p>消費税(10%): <?= $tax_amount ?>円</p>
-    <p><strong>合計金額: <?= $total ?>円</strong></p>
-    
+    <div>
+        <label for="quantity">数量:</label>
+        <?= $quantity; ?>個
+    </div>
+
+    <div>
+        <label for="subtotal">小計:</label>
+        <?= number_format($subtotal); ?>円
+    </div>
+
+    <div>
+        <label for="consumptionRate">消費税(10%):</label>
+        <?= number_format($consumptionRate) ?>円
+    </div>
+
+    <div>
+        <strong>
+            <label for="total">合計金額:</label>
+            <?= number_format($total) ?>円
+        </strong>
+    </div>
+
 </body>
 </html>
